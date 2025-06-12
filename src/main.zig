6f3e15c52d9b8d682a881316871e694322e22026
Halfdan <halfdansc@gmail.com>
2025-06-12 10:08:45 +0200

    started working on get requests

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const stdout = @import("std").io.getStdOut().writer();
 
 const socket_config = @import("server/config.zig");
+const Request = @import("server/request.zig");
 
 pub fn main() !void {
     try stdout.print("Hello, World!\n", .{});
@@ -10,5 +11,8 @@ pub fn main() !void {
     try stdout.print("Server address: {any}", .{socket._address});
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    _ = connection;
+    var buffer: [1000]u8 = [_]u8{0} ** 1000;
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+
+    try stdout.print("{s}", .{buffer});
 }
---
+++ src/server/http_method.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const Map = @import("std").static_string_map.StaticStringMap;
+
+pub const Method = enum {
+    get,
+
+    pub fn init(text: []const u8) !Method {
+        return MethodMap.get(text).?;
+    }
+
+    pub fn is_supported(m: []const u8) bool {
+        const method = MethodMap.get(m);
+        if (method) |_| {
+            return true;
+        }
+        return false;
+    }
+};
+
+const MethodMap = Map(Method).initComptime(
+    .{ "GET", Method.get },
+);
