85d350e9c4d728d4f6dea82fcc61630388b065cb
Halfdan <halfdansc@gmail.com>
2025-06-12 09:32:20 +0200

    socket config setup

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,14 @@
 const std = @import("std");
+const stdout = @import("std").io.getStdOut().writer();
+
+const socket_config = @import("server/config.zig");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
     try stdout.print("Hello, World!\n", .{});
+
+    const socket = try socket_config.Socket.init();
+    try stdout.print("Server address: {any}", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    _ = connection;
 }
--- src/server/config.zig
+++ src/server/config.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const net = @import("std").net;
 
-const Socket = struct {
+pub const Socket = struct {
     _address: std.net.Address,
-    _output_stream: std.net.Stream,
+    _stream: std.net.Stream,
 
-    pub fn init() !void {
+    pub fn init() !Socket {
         // loop back address for host
-        const host = [4]u8{ 123, 0, 0, 1 };
+        const host = [4]u8{ 127, 0, 0, 1 };
         const port: u16 = 3490;
         const addr = net.Address.initIp4(host, port);
         const socket = try std.posix.socket(
@@ -16,6 +16,6 @@ const Socket = struct {
             std.posix.IPPROTO.TCP,
         );
         const stream = net.Stream{ .handle = socket };
-        return Socket{ ._address = addr, ._output_stream = stream };
+        return Socket{ ._address = addr, ._stream = stream };
     }
 };
